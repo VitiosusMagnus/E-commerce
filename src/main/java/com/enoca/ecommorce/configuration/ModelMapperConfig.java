d7937d6633c1628c6141366e082f387f1cbff70c
fokal <furkancan.okal@gmail.com>
2024-08-06 11:45:50 +0300

    fix: configuration annotation

--- src/main/java/com/enoca/ecommorce/configuration/ModelMapperConfig.java
+++ src/main/java/com/enoca/ecommorce/configuration/ModelMapperConfig.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ModelMapperConfig {
 
     @Bean
