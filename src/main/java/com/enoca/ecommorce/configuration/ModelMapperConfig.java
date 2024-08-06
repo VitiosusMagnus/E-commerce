a5c88f13bb33e4e48bee0356573dbc4a9497dfc2
fokal <furkancan.okal@gmail.com>
2024-08-06 11:24:21 +0300

    feat: create modelMapper

---
+++ src/main/java/com/enoca/ecommorce/configuration/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.enoca.ecommorce.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setSkipNullEnabled(true);
+        return modelMapper;
+    }
+}
